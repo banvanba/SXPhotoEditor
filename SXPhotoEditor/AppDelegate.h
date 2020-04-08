6dd2dfb78cff7c81cfd160cc4dea9b7e99065587
Ba Ban <ba.ban@unseen.is>
2020-04-08 09:02:42 +0700

    Initial Commit

---
+++ SXPhotoEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SXPhotoEditor
+//
+//  Created by Ba Ban on 4/8/20.
+//  Copyright © 2020 Ba Ban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
